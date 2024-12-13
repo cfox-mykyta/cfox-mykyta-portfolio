6b7a17947ffdb117abe3004ef6be6df91ea1b365
cfox-mykyta <cfox.mykyta@gmail.com>
2024-12-13 18:30:35 +0200

    Menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,11 @@
-import React from 'react';
 import '../styles/globals.css';
-
-// Мета-дані для сайту
-export const metadata = {
-  title: 'My Portfolio',
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        {/* Підключення шрифтів */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="min-h-screen flex flex-col">
+      <body className="">
         {children}
       </body>
     </html>
   );
 }
-
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,13 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
-
-const Clock: React.FC = () => {
-  const [time, setTime] = useState<string>('');
-
-  useEffect(() => {
-    const updateClock = () => {
-      const kyivTime = new Date().toLocaleTimeString('uk-UA', {
-        timeZone: 'Europe/Kyiv',
-        hour12: false,
-      });
-      setTime(kyivTime);
-    };
-
-    updateClock(); // Update immediately
-    const intervalId = setInterval(updateClock, 1000); // Update every second
-
-    return () => clearInterval(intervalId); // Clean up interval on unmount
-  }, []);
-
-  return (
-    <h1 className="text-9xl font-extrabold tracking-widest" style={{ fontFamily: 'Roboto Mono, monospace' }}>
-      {time}
-    </h1>
-  );
-};
+import Tabs from '../components/Tabs';  // Імпортуємо компонент Tabs
 
 const Page: React.FC = () => {
   return (
-    <main>
-      <Clock />
+    <main className="min-h-screen bg-gray-900 text-white">
+      <Tabs />
     </main>
   );
 };
 
 export default Page;
+
 
