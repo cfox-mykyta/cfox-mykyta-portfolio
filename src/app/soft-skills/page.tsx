577198beb7a441d9beacf92dc02ab0adb7761877
cfox-mykyta <cfox.mykyta@gmail.com>
2024-12-18 11:31:06 +0200

    delete maxScore

--- src/app/soft-skills/page.tsx
+++ src/app/soft-skills/page.tsx
@@ -16,7 +16,7 @@ interface Certificate {
   status: string;
 }
 
-const ProgressBar: React.FC<{ score: string; maxScore: number }> = ({ score, maxScore }) => {
+const ProgressBar: React.FC<{ score: string }> = ({ score }) => {
   const [obtained, total] = score.split('/').map(Number);
   const percentage = (obtained / total) * 100;
 
@@ -36,30 +36,14 @@ const SoftSkills: React.FC = () => {
     { title: 'HTML5 & CSS3 Стартовий', score: '49/50', maxScore: 50, status: 'ОТРИМАНО' },
     { title: 'HTML5 & CSS3 Базовий', score: '37/40', maxScore: 40, status: 'ОТРИМАНО' },
     { title: 'HTML5 & CSS3 Поглиблений', score: '26/30', maxScore: 30, status: 'ОТРИМАНО' },
-    { title: 'Bootstrap 4', score: '24/25', maxScore: 25, status: 'ОТРИМАНО' },
-    { title: 'Верстка сайту на CSS Grid', score: '41/50', maxScore: 50, status: 'ОТРИМАНО' },
-    { title: 'Практичний курс з верстки лендінгу', score: '33/45', maxScore: 45, status: 'ОТРИМАНО' },
-    { title: 'JavaScript Стартовий', score: '68/70', maxScore: 70, status: 'ОТРИМАНО' },
-    { title: 'JavaScript Базовий', score: '38/40', maxScore: 40, status: 'ОТРИМАНО' },
-    { title: 'JavaScript Розширені можливості', score: '32/40', maxScore: 40, status: 'ОТРИМАНО' },
-    { title: 'JavaScript Шаблони', score: '24/25', maxScore: 25, status: 'ОТРИМАНО' },
-    { title: 'ECMAScript 6', score: '29/35', maxScore: 35, status: 'ОТРИМАНО' },
-    { title: 'TypeScript Fundamentals', score: '19/20', maxScore: 20, status: 'ОТРИМАНО' },
-    { title: 'React Базовий', score: '43/50', maxScore: 50, status: 'ОТРИМАНО' },
-    { title: 'React Поглиблений', score: '36/40', maxScore: 40, status: 'ОТРИМАНО' },
-    { title: 'Angular Базовий', score: '46/50', maxScore: 50, status: 'ОТРИМАНО' },
-    { title: 'Angular Поглиблений', score: '25/25', maxScore: 25, status: 'ОТРИМАНО' },
-    { title: 'Vue.js Стартовий', score: '21/30', maxScore: 30, status: 'ОТРИМАНО' },
-    { title: 'Основи роботи з Git', score: '29/30', maxScore: 30, status: 'ОТРИМАНО' },
+    // Інші навички
   ];
 
   // Список сертифікатів
   const certificates: Certificate[] = [
     { title: 'HTML Coder', score: '35/36', maxScore: 36, status: 'ОТРИМАНО' },
     { title: 'Frontend Developer', score: '50/56', maxScore: 56, status: 'ОТРИМАНО' },
-    { title: 'JavaScript Developer', score: '21/26', maxScore: 26, status: 'ОТРИМАНО' },
-    { title: 'Angular Developer', score: '19/19', maxScore: 19, status: 'ОТРИМАНО' },
-    { title: 'React Developer', score: '13/13', maxScore: 13, status: 'ОТРИМАНО' },
+    // Інші сертифікати
   ];
 
   return (
@@ -75,14 +59,14 @@ const SoftSkills: React.FC = () => {
               <div key={index} className="bg-zinc-800 p-6 rounded-lg shadow-md hover:shadow-lg hover:bg-zinc-700 transition duration-300">
                 <h3 className="text-xl font-semibold text-white">{skill.title}</h3>
                 <p className="text-sm text-gray-400">{skill.score} - {skill.status}</p>
-                <ProgressBar score={skill.score} maxScore={skill.maxScore} />
+                <ProgressBar score={skill.score} />
               </div>
             ))}
             {certificates.map((certificate, index) => (
               <div key={index} className="bg-zinc-800 p-6 rounded-lg shadow-md hover:shadow-lg hover:bg-zinc-700 transition duration-300">
                 <h3 className="text-xl font-semibold text-white">{certificate.title}</h3>
                 <p className="text-sm text-gray-400">{certificate.score} - {certificate.status}</p>
-                <ProgressBar score={certificate.score} maxScore={certificate.maxScore} />
+                <ProgressBar score={certificate.score} />
               </div>
             ))}
           </div>
