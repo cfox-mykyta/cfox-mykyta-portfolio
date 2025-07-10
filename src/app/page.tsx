318f42d64199f0ee7a6ed63190d1f32ab443af6e
cfox-mykyta <cfox.mykyta@gmail.com>
2025-07-10 14:38:33 +0300

    Deleted unused Footer and Header imports to clean up code and avoid repetition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Motivation from '../components/Motivation';
-import Footer from '../components/Footer';
 
 const Page: React.FC = () => {
   return (
     <>
       <div className='bg-zinc-900'>
-        <Header />
         <Hero />
         <Motivation />
-        <Footer />
       </div>
     </>
   );
